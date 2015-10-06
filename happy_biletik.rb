7d52e1eb9afd400182c7aa2644ce321e4a7d7993
Anakonda <speed-help@mail.ru>
2015-10-06 19:08:18 +0500

    first commit

--- happy_biletik.rb
+++ happy_biletik.rb
@@ -1,18 +1,12 @@
 z = ARGV[0].split""
-a = z[0]
-b = z[1]
-c = z[2]
-d = z[3]
-f = z[4]
-g = z[5]
 summa1 = a.to_i + b.to_i + c.to_i
 summa2 = d.to_i + f.to_i + g.to_i
 if summa1 > summa2
   puts 'No success'
-end
-if summa1 == summa2
+else
+   summa1 == summa2
   puts 'Success'
-end
-if summa1 < summa2
+else
+   summa1 < summa2
   puts 'No success'
 end
