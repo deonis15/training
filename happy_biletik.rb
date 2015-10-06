70e35ca7a648c9312192e9691709f82d6cdcbef1
Anakonda <speed-help@mail.ru>
2015-10-06 19:45:38 +0500

    happy-biletik

--- happy_biletik.rb
+++ happy_biletik.rb
@@ -1,12 +1,8 @@
 z = ARGV[0].split""
-summa1 = a.to_i + b.to_i + c.to_i
-summa2 = d.to_i + f.to_i + g.to_i
-if summa1 > summa2
-  puts 'No success'
-else
-   summa1 == summa2
+summa1 = z[0].to_i + z[1].to_i + z[2].to_i
+summa2 = z[3].to_i + z[4].to_i + z[5].to_i
+if summa1 == summa2
   puts 'Success'
 else
-   summa1 < summa2
-  puts 'No success'
+  puts 'unsuccess'
 end
